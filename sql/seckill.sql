4885f51863bbc565967d91c2e8a53b4b5b31caa4
lirchis <2955908973@qq.com>
2023-03-09 17:04:04 +0800

    重新提交

--- sql/seckill.sql
+++ sql/seckill.sql
@@ -1,17 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : localhost
-Source Server Version : 50720
-Source Host           : localhost:3306
-Source Database       : seckill
-
-Target Server Type    : MYSQL
-Target Server Version : 50720
-File Encoding         : 65001
-
-Date: 2018-05-29 17:29:21
-*/
 
 SET FOREIGN_KEY_CHECKS=0;
 
